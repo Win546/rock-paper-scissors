19c6baaa8d8ee88d0f31d52f60487f929314b029
Win546 <91315687+Win546@users.noreply.github.com>
2025-04-18 13:15:43 +0200

    Writed logic of getComputerChoice
    I've added a qay for the computer to make a choice

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("ciao")
+
+
+function getComputerChoice(){
+    console.log(Math.random());
+    let random=Math.random();
+    let choice=""
+    if(random < 0.33 ){
+choice="Rock";
+    } else if (random >= 0.33 && random <= 0.66){
+        choice="Paper";
+    } else {
+        choice="Scissors";
+        }
+        return choice;
+}
+
+let computerChoice=getComputerChoice();
